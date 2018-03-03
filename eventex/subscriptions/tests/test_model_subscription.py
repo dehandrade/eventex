5d2c2f63c1caf9095823d5cd60369425ab4612f3
André Andrade <andreandrade042@gmail.com>
2018-03-03 16:22:22 -0300

    Admin magic

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -14,9 +14,15 @@ class SubscriptionModelTest(TestCase):
         )
         self.obj.save()
 
+
     def test_create(self):
         self.assertTrue(Subscription.objects.exists())
+
 
     def test_created_at(self):
         """Subscription must have an auto created_at attr."""
         self.assertIsInstance(self.obj.created_at, datetime)
+
+
+    def test_str(self):
+        self.assertEqual(self.obj.name, str(self.obj))
