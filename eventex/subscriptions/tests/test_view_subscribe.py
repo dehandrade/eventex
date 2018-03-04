05490b0a4e83de52f3a7dec2aca40895efc6948a
André Andrade <andreandrade042@gmail.com>
2018-03-04 10:51:21 -0300

    Thank you page

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,3 +1,5 @@
+import unittest
+
 from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
@@ -12,11 +14,9 @@ class SubscribeGet(TestCase):
         """Get /inscricao/ must return status code 200"""
         self.assertEqual(200, self.resp.status_code)
 
-
     def test_template(self):
         """Must use subscriptions/subscription_form.html"""
         self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
-
 
     def test_html(self):
         """HTML must contain input tags"""
@@ -30,11 +30,9 @@ class SubscribeGet(TestCase):
             with self.subTest():
                 self.assertContains(self.resp, text, count)
 
-
     def test_csrf(self):
         """HTML must contain csrf"""
         self.assertContains(self.resp, 'csrfmiddlewaretoken')
-
 
     def test_has_form(self):
         """Context must have subscription form"""
@@ -48,15 +46,12 @@ class SubscribePostValid(TestCase):
                     email='henrique@bastos.net', phone='21-99618-6180')
         self.resp = self.client.post('/inscricao/', data)
 
-
     def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
-
+        """Valid POST should redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
-
 
     def test_save_subscription(self):
         self.assertTrue(Subscription.objects.exists())
@@ -66,34 +61,20 @@ class SubscribePostInvalid(TestCase):
     def setUp(self):
         self.resp = self.client.post('/inscricao/', {})
 
-
     def test_post(self):
         """Invalid POST should not redirect"""
         self.assertEqual(200, self.resp.status_code)
 
-
     def test_template(self):
         self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
-
 
     def test_has_form(self):
         form = self.resp.context['form']
         self.assertIsInstance(form, SubscriptionForm)
 
-
     def test_form_has_errors(self):
         form = self.resp.context['form']
         self.assertTrue(form.errors)
 
-
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Henrique Bastos', cpf='12345678901',
-                    email='henrique@bastos.net', phone='21-99618-6180')
-
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
