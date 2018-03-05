cfbe44022da47d7211e136b5fda45592e407c944
André Andrade <andreandrade042@gmail.com>
2018-03-05 19:21:08 -0300

    Extrai urls hardcoded

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -1,21 +1,20 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
 
 class HomeTest(TestCase):
     def setUp(self):
-        self.response = self.client.get('/')
-
+        self.response = self.client.get(r('home'))
 
     def test_get(self):
         """GET / must return status code 200"""
         self.assertEqual(200, self.response.status_code)
-
 
     def test_template(self):
         """Must use index.html"""
         response = self.client.get('/')
         self.assertTemplateUsed(self.response, 'index.html')
 
-
     def test_subscription_link(self):
-        self.assertContains(self.response, 'href="/inscricao/"')
+        expected = 'href="{}"'.format(r('subscriptions:new'))
+        self.assertContains(self.response, expected)
